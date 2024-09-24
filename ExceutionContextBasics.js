cba796965192eaba620d6550fdccc315817196d0
Aniket Mogare <aniketmogare45@gmail.com>
2024-09-24 08:12:36 +0530

    24 sept commit 1

---
+++ ExceutionContextBasics.js
@@ -0,0 +1,15 @@
+console.log(this);
+console.log(window);
+console.log(myFunction);
+console.log(fullName);
+
+function myFunction(){
+    console.log("this is my function");
+}
+
+var firstName="Aniket";
+var lastName="Mogare";
+var fullName=firstName + " " + lastName;
+console.log(fullName);
+// refer GLOBAL EXECUTION CONTECT.DOCS
+
