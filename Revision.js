1e72a243723a4ca9ecd074fa72213e50ed1eef7f
Aniket Mogare <aniketmogare45@gmail.com>
2024-09-27 09:12:07 +0530

    27 sept commit

--- Revision.js
+++ Revision.js
@@ -1,1 +1,13 @@
-console.log("hi");
+console.log(this);
+console.log(window);
+console.log(myFunction);
+console.log(fullName);
+
+function myFunction(){
+    console.log("this is my function");
+}
+
+var firstName="Aniket";
+var lastName="Mogare";
+var fullName=firstName +" "+ lastName;
+console.log(fullName);
