016c5d73348c610554a20b32c68e0e588f2701d2
Aniket Mogare <aniketmogare45@gmail.com>
2024-10-01 09:07:12 +0530

    1-oct-24

--- DOM_Basics/dom_1js.js
+++ DOM_Basics/dom_1js.js
@@ -12,8 +12,27 @@ console.log(typeof document.getElementById("main-heading"));
 const mainHeading=document.querySelector("div.headline h2");
 // mainHeading.textContent="this is something element";
 // console.log(mainHeading.style);//returns style object
-mainHeading.style.color="blue";
-mainHeading.style.border="10px solid orange";
+// mainHeading.style.color="blue"; // changes text color 
+// mainHeading.style.border="10px solid orange";// changes border
 
 
-const navItems=document.querySelectorAll(".nav-items a")
+//get and set Attributes
+// const link=document.querySelector("a");
+// link.setAttribute("href","#hometest");
+// console.log(link.getAttribute("href"));
+
+// const inputElement=document.querySelector(".form-todo input");
+// console.log(inputElement.getAttribute("type"));
+
+//get multiple elements using get element by class Name
+// const navItems=document.getElementsByTagName("a");
+// for(nav of navItems){
+//     nav.style.color="green";
+//     nav.style.backgroundColor="white";
+//     nav.style.fontWeight="bold";
+// }
+const headline=document.querySelector(".headline");
+// console.log(headline.innerHTML);
+headline.innerHTML="<h1>Inner HTML cnanged</h1>";
+headline.innerHTML+="<button class=\"btn\" >Learn More</button>";
+
