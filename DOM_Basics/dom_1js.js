1adb0e36bf51543956c418944fb13d7287850476
Aniket Mogare <aniketmogare45@gmail.com>
2024-10-02 08:41:12 +0530

    02-10-24

--- DOM_Basics/dom_1js.js
+++ DOM_Basics/dom_1js.js
@@ -30,9 +30,35 @@ const mainHeading=document.querySelector("div.headline h2");
 //     nav.style.color="green";
 //     nav.style.backgroundColor="white";
 //     nav.style.fontWeight="bold";
-// }
-const headline=document.querySelector(".headline");
-// console.log(headline.innerHTML);
-headline.innerHTML="<h1>Inner HTML cnanged</h1>";
-headline.innerHTML+="<button class=\"btn\" >Learn More</button>";
+// // }
+// const headline=document.querySelector(".headline");
+// // console.log(headline.innerHTML);
+// headline.innerHTML="<h1>Inner HTML cnanged</h1>";
+// headline.innerHTML+="<button class=\"btn\" >Learn More</button>";
+const  sectionTodo=document.querySelector(".section-todo");
+// console.log(sectionTodo.classList);
+// sectionTodo.classList.add("bg-dark"); /// adds a class to classList
+// sectionTodo.classList.remove("bg-dark"); // removes a class from classList
+// const ans=sectionTodo.classList.contains("container"); //checks a class is present in classList or not 
+// console.log(ans);
+
+// sectionTodo.classList.toggle("bg-dark");   // if not present it will add vise versa
+// sectionTodo.classList.toggle("bg-dark"); // if added it will remove and vice versa
+
+// const header=document.querySelector(".header");
+// console.log(header.classList);
+// header.classList.add("bg-dark");             //learn specificity IN CSS
+// header.classList.toggle("bg-dark");
+
+//document.createElement()
+//append
+//prepend
+//remove
+
+const newTodoItem=document.createElement("li");
+const newTodoItemText=document.createTextNode("Write a blog");
+const todoList=document.querySelector(".todo-list");
+newTodoItem.append(newTodoItemText);
+todoList.append(newTodoItem);
+
 
