14e230430835f7fff9386ff8561630bbc3917cbb
Aniket Mogare <aniketmogare45@gmail.com>
2024-10-14 23:37:00 +0530

    14-oct-24

--- Revision.js
+++ Revision.js
@@ -9,18 +9,4 @@ const user2={
     firstName:"Shubham",
     age:26,
 }
-const user1={
-    firstName:"Aniket",
-    age:"24",  
-    about(){
-        console.log(this.firstName,this.age);
-    }
-}
-const user2={
-    firstName:"Shubham",
-    age:26,
-}
-
-
-
 
